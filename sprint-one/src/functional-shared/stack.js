82c475aae5c2aa80b7c4eaa240d37203d766b848
mickeymcc <mccmarci@gmail.com>
2018-01-17 12:08:46 -0600

    Pass test size of 0

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {'length': 0, 'storage': {}};
+
+  var extend = function(obj1, obj2) {
+    for (var key in obj2) {
+      obj1[key] = obj2[key];
+    }
+  }
+  
+  extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function (value) {},
+
+  pop: function() {},
+
+  size: function() {
+      return this.length;
+    }
+
+};
 
 
