a352183fdf302625d01a5353c65fdc5cbc89f01b
mickeymcc <mccmarci@gmail.com>
2018-01-17 16:08:42 -0600

    Pass all es6 Pattern for stacks

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,26 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.length = 0;
+    this.storage = {};
+  }
+
+  push(value) {
+    this.length++;
+    this.storage[this.length] = value;
+  }
+  
+  pop() {
+    var popped;
+    if (this.length > 0) {
+      popped = this.storage[this.length];
+      this.length--;
+    }
+    return popped;
+  }
+
+  size() {
+    return this.length;
   }
 
 }
