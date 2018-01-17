aece38e11257279be5125c091ec27db64343d67d
mickeymcc <mccmarci@gmail.com>
2018-01-17 15:26:07 -0600

    Pass report size of 0 for prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.length = 0;
+  instance.storage = {};
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
 
+};
+
+stackMethods.pop = function() {
+};
+
+stackMethods.size = function() {
+  return this.length;
+};
