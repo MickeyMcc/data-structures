8b2071dab43bd4602106e9af5218742f7b8d8bcd
mickeymcc <mccmarci@gmail.com>
2018-01-17 12:17:42 -0600

    Pass functional-shared-instantiation style test

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var anInstance = {'size': 0, 'storage': {}};
+
+  var extend = function(obj1, obj2) {
+    for (var key in obj2) {
+      obj1[key] = obj2[key];
+    }
+  };
+
+  extend(anInstance, queueMethods);
+
+  return anInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function() {},
+
+  dequeue: function() {},
+
+  size: function() {}
+};
 
 
