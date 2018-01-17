39d4ddea5e7248c22005361f6c6d6df31e6fdfa7
mickeymcc <mccmarci@gmail.com>
2018-01-17 15:12:42 -0600

    Use _.extend

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,13 +3,7 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   var anInstance = {'length': 0, 'storage': {}};
 
-  var extend = function(obj1, obj2) {
-    for (var key in obj2) {
-      obj1[key] = obj2[key];
-    }
-  };
-
-  extend(anInstance, queueMethods);
+  _.extend(anInstance, queueMethods);
 
   return anInstance;
 };
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,14 +2,8 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {'length': 0, 'storage': {}};
-
-  var extend = function(obj1, obj2) {
-    for (var key in obj2) {
-      obj1[key] = obj2[key];
-    }
-  };
   
-  extend(someInstance, stackMethods);
+  _.extend(someInstance, stackMethods);
   return someInstance;
 };
 
