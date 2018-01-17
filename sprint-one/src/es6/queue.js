316ac8ec39859c16fa97d8aa00ad286ca6060ce4
mickeymcc <mccmarci@gmail.com>
2018-01-17 16:13:26 -0600

    Pass all es6 pattern tests for queues

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,28 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.length = 0;
+    this.storage = {};
   }
 
+  enqueue(value) {
+    this.length++;
+    this.storage[this.length] = value;
+  }
+  
+  dequeue() {
+    var next;
+    if (this.length > 0) {
+      next = this.storage[1];
+      for (var i = 1; i <= this.length; i++) {
+        this.storage[i] = this.storage[i + 1];
+      }
+      this.length--;
+    }
+    return next;
+  }
+
+  size() {
+    return this.length;
+  }
 }
