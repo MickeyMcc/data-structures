89b468197301982677f00c152ee3fc91d2efda07
mickeymcc <mccmarci@gmail.com>
2018-01-18 15:57:37 -0600

    Pass all graph tests

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -9,7 +9,7 @@ var Graph = function(value) {
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
   this.nodes.push(node);
-  this.edges.push({node : []});
+  this.edges[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
@@ -19,30 +19,39 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  nodeIndex = this.nodes.indexOf(node);
+  var nodeIndex = this.nodes.indexOf(node);
   this.nodes.splice(nodeIndex, 1);
-/*
-  this.edges[nodeIndex].node.forEach(function(connected) {
-      connectedIndex = 
-      this.nodes.splice(connectedIndex, 1);
+  for (var i = 0; i < this.edges[node].length; i++){ 
+    this.removeEdge(node, this.edges[node][i]);
   }
-*/
+  this.edges[node] = [];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.edges[fromNode].includes(toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges[fromNode].push(toNode);
+  this.edges[toNode].push(fromNode);
+
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var nodeIndex = this.edges[fromNode].indexOf(toNode);
+  this.edges[fromNode].splice(nodeIndex,1);
+  nodeIndex = this.edges[toNode].indexOf(fromNode);
+  this.edges[toNode].splice(nodeIndex,1);
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for(var i = 0; i < this.nodes.length; i++) {
+    cb(this.nodes[i]);
+  }
 };
 
 /*
