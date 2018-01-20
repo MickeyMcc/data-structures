f3dca897a8f072de3172c52af75bf60ad9ac1829
mickeymcc <mccmarci@gmail.com>
2018-01-20 14:57:36 -0600

    Refactor hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -10,60 +10,81 @@ HashTable.prototype.insert = function(k, v, shifting) {
   if ((this._numberOfThings + 1) / this._limit >= 0.75) {
     this.changeSize(2); 
   }
+
   var index = getIndexBelowMaxForKey(k, this._limit);
   var currentBucket = this._storage.get(index);
-  this._storage.set(index, this.addToBucket(currentBucket, k, v));
-  this._numberOfThings++;
+  
+  if (!currentBucket) {
+    currentBucket = [];
+    this._storage.set(index, currentBucket);
+  } 
+
+  var found = false;
+
+  for (var i = 0; i < currentBucket.length; i++) {
+    var tuple = currentBucket[i];
+    if (tuple[0] === k) {
+      tuple[1] = v;
+    }
+  }
+  
+  if (!found) {
+    currentBucket.push([k, v]);
+    this._numberOfThings++;
+  }
 };
 
-HashTable.prototype.addToBucket = function(bucket, k, v) {
-  if (Array.isArray(bucket)) { //bucket has been previously used
-    for (var i = 0; i < bucket.length; i++) {
-      if (bucket[i][0] === k) { //key already stored in hash table
-        bucket[i][1] = v; //update to new value
-      } else { //new key
-        bucket.push([k, v]); //add value pair to bucket
-      }
-    }
-  } else {
-    bucket = [[k, v]]; //new bucket
-  }
-  return bucket;
-};
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index); //bucket is arr of tuples
-  return this.findInBucket(bucket, k);
-};
+  if (!bucket) {
+    return null;
+  } 
 
-HashTable.prototype.findInBucket = function(bucket, k) {
   for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
-      return bucket[i][1];
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
     }
   }
+  
+  return null;
 };
 
 HashTable.prototype.remove = function(k) {
   if ((this._numberOfThings - 1) / this._limit < 0.25) {
     this.changeSize(.5); 
   }
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var holderBucket = this._storage.get(index) || [];
-  holderBucket = this.removeFromBucket(holderBucket, k);
-  this._storage.set(index, holderBucket); //set storage to be new smaller bucket
-  this._numberOfThings--;
-};
 
-HashTable.prototype.removeFromBucket = function(bucket, k) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index); //bucket is arr of tuples
+
+  if (!bucket) {
+    return null;
+  } 
+  
+  //iterate through bucket
   for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) { //find key in bucketed tuples
-      bucket.splice(i, 1); //take that tuple out of bucket
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      bucket.splice(i, 1);
+      this._numberOfThings--;
+      return tuple[1];
     }
   }
-  return bucket;
+  
+  return null;
 };
+
+// HashTable.prototype.removeFromBucket = function(bucket, k) {
+//   for (var i = 0; i < bucket.length; i++) {
+//     if (bucket[i][0] === k) { //find key in bucketed tuples
+//       bucket.splice(i, 1); //take that tuple out of bucket
+//     }
+//   }
+//   return bucket;
+// };
 
 HashTable.prototype.findKeyFor = function(v) {
   for (var i = 0; i < this._limit; i++) {
@@ -88,15 +109,14 @@ HashTable.prototype.findKeyInBucket = function(bucket, v) {
 
 HashTable.prototype.pullEverything = function() {
   var tempStorage = [];
-  for (var i = 0; i < (this._limit); i++) {
-    var oldBucket = this._storage.get(i);
-    if (oldBucket) { // for unit of existing storage
-      for (var j = 0; j < oldBucket.length; j++) { //for each tuple in the bucket there
-        tempStorage.push(oldBucket[j]);
+  this._storage.each(function (bucket) {
+    if (bucket) { // for unit of existing storage
+      for (var j = 0; j < bucket.length; j++) { //for each tuple in the bucket there
+        tempStorage.push(bucket[j]);
       }
     }
-    this._storage.set(i, []); //clears storage in prep for adding everything back in
-  }
+    bucket = []; //clears storage in prep for adding everything back in
+  }.bind(this));
   return tempStorage;
 };
 
